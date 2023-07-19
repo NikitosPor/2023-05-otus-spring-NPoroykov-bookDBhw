2bc5a6732b27e80c6b417632d4618653cd170a2e
Nikita P <nporoykov@gmail.com>
2023-07-19 16:04:43 +0300

    initial commit

---
+++ src/main/java/ru/otus/bookdborm/BookdbormApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.bookdborm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookdbormApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookdbormApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/otus/bookdborm/BookdbormApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.bookdborm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookdbormApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
